330209e53e3dd3defb11f765a34ef1133577ced2
IrhamIzza <Irhamizza21@gmail.com>
2025-05-09 10:23:09 +0700

    Add AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,21 @@
+<script setup>
+import Footer from '@/components/Footer.vue';
+
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <section>
+        <div class="bg-black text-white p-4">
+            <h1 class="text-4xl font-bold text-center py-4">TENTANG KAMI</h1>
+            <div class="flex flex-col gap-2 text-1xl md:text-2xl">
+                <p>Kami adalah perusahaan pupuk pertanian dalam mengatasi kelangkaan pupuk subsidi dari pemerintah. Kami menyediakan pupuk alternatif yang berkualitas dengan harga yang terjangkau oleh masyarakat petani,Karakter pupuk kami adalah mengandung unsur yang lengkap dan seimbang antara unsur makro dan mikro sesuai dengan yang di butuhkan tanaman.</p>
+                <p>Produk kami sudah teruji sejak tahun 1988, Dari pengalaman kami di lapangan pupuk kami bisa meningkat hasil panen sekitar 20% . Tentu nya dengan standar dosis kami sesuai dengan jenis tanaman. Kami berkomitmen memberikan kepuasan petani berupa kesehatan tanaman dan peningkatan hasil panen. Kami memberikan garansi mutu yang bisa di tuangkan dalam perjanjian tertulis dengan petani/konsumen. Dengan catatan bukan di atas tanah yang selalu mengeluarkan air dan tidak bisa mengering ( bahasa Jawa tanah becer)</p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <Footer></Footer>
+    </section>
 </template>
 
 <style>
-/* @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-} */
 </style>
