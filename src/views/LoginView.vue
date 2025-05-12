accc364035638e8879a6cd18ffea079e2276f5e5
IrhamIzza <Irhamizza21@gmail.com>
2025-05-12 08:43:27 +0700

    add form in loginview

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,43 +1,45 @@
 <template>
     <section class="bg-light py-3">
-        <Card class="mx-auto max-w-sm my-[15vh]">
-            <CardHeader>
-                <CardTitle class="text-2xl">
-                    Login
-                </CardTitle>
-                <CardDescription>
-                    Enter your email below to login to your account
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <div class="grid gap-4">
-                    <div class="grid gap-2">
-                        <Label for="email">Email</Label>
-                        <Input v-model="email" id="email" type="email" placeholder="m@example.com" required />
-                    </div>
-                    <div class="grid gap-2">
-                        <div class="flex items-center">
-                            <Label for="password">Password</Label>
-                            <a href="#" class="ml-auto inline-block text-sm underline">
-                                Forgot your password?
-                            </a>
-                        </div>
-                        <Input v-model="password" id="password" type="password" required />
-                    </div>
-                    <Button @click="login" type="submit" class="w-full">
+        <form @submit.prevent="login">
+            <Card class="mx-auto max-w-sm my-[15vh]">
+                <CardHeader>
+                    <CardTitle class="text-2xl">
                         Login
-                    </Button>
-                    <div v-if="errorMessage" class="text-sm text-red-600">
-                        {{ errorMessage }}
+                    </CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div class="grid gap-4">
+                        <div class="grid gap-2">
+                            <Label for="email">Email</Label>
+                            <Input v-model="email" id="email" type="email" placeholder="m@example.com" required />
+                        </div>
+                        <div class="grid gap-2">
+                            <div class="flex items-center">
+                                <Label for="password">Password</Label>
+                                <!-- <a href="#" class="ml-auto inline-block text-sm underline">
+                                    Forgot your password?
+                                </a> -->
+                            </div>
+                            <Input v-model="password" id="password" type="password" autocomplete="on" required />
+                        </div>
+                        <Button @click="login" type="submit" class="w-full">
+                            Login
+                        </Button>
+                        <div v-if="errorMessage" class="text-sm text-red-600">
+                            {{ errorMessage }}
+                        </div>
                     </div>
-                </div>
-                <div class="mt-4 text-center text-sm">
-                    Don't have an account?
-                    <router-link to=/register class="underline"> Sign up
-                    </router-link>
-                </div>
-            </CardContent>
-        </Card>
+                    <div class="mt-4 text-center text-sm">
+                        Don't have an account?
+                        <router-link to=/register class="underline"> Sign up
+                        </router-link>
+                    </div>
+                </CardContent>
+            </Card>
+        </form>
     </section>
 </template>
 
@@ -65,7 +67,6 @@ onMounted(() => {
     if (token != null) {
         router.push('/home');
     }
-    console.log(import.meta.env.VITE_API_URL)
 });
 
 function login() {
@@ -87,7 +88,6 @@ function login() {
             console.log(error.response.data.messages);
             errorMessage.value = error.response.data.messages;
             console.error(error);
-            console.error(error.response);
         });
 
 }
